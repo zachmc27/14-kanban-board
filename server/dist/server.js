82f7f060f05195e4b6be5d383eaed8ac9828255a
zachmc27 <dzdudes1213@gmail.com>
2025-03-24 14:53:27 -0500

    fixed path to dist

--- server/dist/server.js
+++ server/dist/server.js
@@ -11,11 +11,11 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3001;
 // Serves static files in the entire client's dist folder
-app.use(express.static(path.resolve(__dirname, "client", "dist")));
+app.use(express.static(path.resolve(__dirname, "../../client/dist")));
 app.use(express.json());
 app.use(routes);
 app.get("*", (_req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../../client/dist", "index.html"));
 });
 sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
     app.listen(PORT, () => {
